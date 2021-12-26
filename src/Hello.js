360431450077513281df2bde28de032b914613dc
Beattris <beatriceebirim@gmail.com>
2021-12-26 20:15:09 +0100

    update

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Hello.css";
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="tc">
+        <h1>Hello world</h1>
+        <p>we're learning react</p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Hello'
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
