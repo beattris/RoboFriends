31cfca3f3f231facf476833d2fc7acc0bfd4f2bd
Beattris <beatriceebirim@gmail.com>
2022-02-15 16:23:13 +0100

    added redux to manage state

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { searchRobots } from "./reducers";
 import "./index.css";
-import App from './containers/App'
+import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 
+const store = createStore(searchRobots);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
